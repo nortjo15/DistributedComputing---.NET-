d68fba05e232be874d961b222e6d59dc69566257
Sean <118966410+aeruza@users.noreply.github.com>
2025-10-12 23:53:36 +0800

    feat(accounts-controller): added get, put, delete api implementations

--- BankWebService/Controllers/AccountController.cs
+++ BankWebService/Controllers/AccountController.cs
@@ -1,5 +1,7 @@
 ﻿using BankWebService.Data;
+using BankWebService.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace BankWebService.Controllers
 {
@@ -14,7 +16,8 @@ namespace BankWebService.Controllers
             _context = context;
         }
 
-        // Create Account
+        // Create Account // TODO
+        // Post: api/account/create_account
         [HttpPost("create_account")]
         public async Task<IActionResult> CreateAccount()
         {
@@ -22,24 +25,80 @@ namespace BankWebService.Controllers
         }
 
         // Get Account by Account Number
-        [HttpGet("{id}")]
-        public async Task<IActionResult> GetAccount()
+        // GET: api/account/{accountNumber}
+        [HttpGet("{accountNumber}")]
+        public async Task<ActionResult<Account>> GetAccount(int accountNumber)
         {
-            return NotFound();
+            if(_context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(accountNumber);
+            if(account == null)
+            {
+                return NotFound();
+            }
+
+            return account;
         }
 
         // Update Account
-        [HttpPut("{id}")]
-        public async Task<IActionResult> UpdateAccount()
+        // PUT: api/account/{accountNumber}
+        [HttpPut("{accountNumber}")]
+        public async Task<IActionResult> UpdateAccount(int accountNumber, Account account)
         {
-            return NotFound();
+            if(accountNumber != account.AccountNumber)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(account).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if(!AccountExists(accountNumber))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
         }
 
         // Delete Account
-        [HttpDelete("{id}")]
-        public async Task<IActionResult> DeleteAccount(int id)
+        // DELETE: api/account/{accountNumber}
+        [HttpDelete("{accountNumber}")]
+        public async Task<IActionResult> DeleteAccount(int accountNumber)
         {
-            return NotFound();
+            if(_context.Accounts == null)
+            {
+                return NotFound();
+            }
+
+            var account = await _context.Accounts.FindAsync(accountNumber);
+            if(account == null)
+            {
+                return NotFound();
+            }
+
+            _context.Accounts.Remove(account);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool AccountExists(int id)
+        {
+            return (_context.Accounts?.Any(e => e.AccountNumber == id)).GetValueOrDefault();
         }
     }
 }
